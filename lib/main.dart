43356063e6918b89bbbb8c5f61ab466e57fc23e3
Aman Gautam <amangautam1905@gmail.com>
2023-09-12 13:43:07 +0530

    Color and Size Change Completed

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:major_project__widget_testing/Navigation.dart';
+import 'package:major_project__widget_testing/widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Navigation(),
+    );
+  }
+}
+
