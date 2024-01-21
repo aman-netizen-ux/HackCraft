c0ae9bb1c205989a57000ce5d1adf9135199c4df
Abha Agarwal <abhaa1023@gmail.com>
2024-01-21 16:51:41 +0530

    image picker partially implemented

--- lib/state/galleryProvider.dart
+++ lib/state/galleryProvider.dart
@@ -1,15 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:logger/logger.dart';
 
 //This provider is for the gallery section.
 class GalleryProvider with ChangeNotifier{
-
+late Uint8List selectedImageInBytes;
   //The demo list is there to show the images in the gallery.
+  // final List<String> _galleryImages = [
+  //   'assets/images/defaultTemplate/GalleryImage.png',
+  //   'assets/images/defaultTemplate/GalleryImage.png',
+  //   // 'assets/images/defaultTemplate/GalleryImage.png',
+  //   // 'assets/images/defaultTemplate/GalleryImage.png',
+  //   // 'assets/images/defaultTemplate/GalleryImage.png'
+  // ];
+
   final List<String> _galleryImages = [
-    'assets/images/defaultTemplate/GalleryImage.png',
-    'assets/images/defaultTemplate/GalleryImage.png',
-    'assets/images/defaultTemplate/GalleryImage.png',
-    'assets/images/defaultTemplate/GalleryImage.png',
-    'assets/images/defaultTemplate/GalleryImage.png'
+    'default','default'
+    // 'assets/images/defaultTemplate/GalleryImage.png',
+    // 'assets/images/defaultTemplate/GalleryImage.png',
+    // 'assets/images/defaultTemplate/GalleryImage.png',
+    // 'assets/images/defaultTemplate/GalleryImage.png',
+    // 'assets/images/defaultTemplate/GalleryImage.png'
   ];
 
   List<String> get galleryImages => _galleryImages;
@@ -17,4 +34,64 @@ class GalleryProvider with ChangeNotifier{
   // void setGalleryImages(String imagePath){
   //   _galleryImages = imagePath;
   // }
+
+  selectFile(bool fileFrom)async{
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    // if(filePickerResult!= null){
+    //   String selectedFile= filePickerResult.files.first.name;
+    //   selectedImageInBytes=filePickerResult.files.first.bytes!;
+    //   String base64String = base64Encode(selectedImageInBytes);
+    //   selectedFile.isEmpty?
+    //   _galleryImages.add('default')
+    //   :_galleryImages.add(base64String)
+    //   ;
+    // }
+
+    if (result != null) {
+  Uint8List fileBytes = result.files.first.bytes!;
+  String fileName = result.files.first.name;
+  print(fileName);
+  
+}
+    notifyListeners();
+
+  }
+
+
+
+  Future<void> pickImage()async {
+    if(!kIsWeb){
+
+      final ImagePicker _imagePicker= ImagePicker();
+      XFile? image= await _imagePicker.pickImage(source: ImageSource.gallery);
+
+      if(image!=null){
+       var selectedFile= File(image.path);
+       print(selectedFile);
+      }
+
+    }
+    
+    else if(kIsWeb){
+      final ImagePicker _imagePicker= ImagePicker();
+      XFile? image= await _imagePicker.pickImage(source: ImageSource.gallery);
+
+      if(image!=null){
+        var f = await image.readAsBytes();
+        Uint8List webImage= f;
+        String base64String = base64Encode(webImage);
+        _galleryImages.add(base64String);
+      }else{
+      var logger = Logger();
+      logger.i("Doesn't picked anything");
+    }
+
+    }else{
+      var logger = Logger();
+      logger.e("Something went wrong");
+    }
+    notifyListeners();
+
+  }
 }
--- lib/views/Screens/DefaultEditPortal/Desktop/Sections/Canvas/default_gallerySection.dart
+++ lib/views/Screens/DefaultEditPortal/Desktop/Sections/Canvas/default_gallerySection.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:major_project__widget_testing/constants/colors.dart';
@@ -41,107 +44,150 @@ class DefaultEditGallerySection extends StatelessWidget {
           SizedBox(height: defaultEditScaleHeight(containerHeight, 94)),
 
           //From here, the images will be shown in the list view using the list made in the gallery provider.
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:
-                  List.generate(galleryProvider.galleryImages.length, (index) {
-                //length of the image list present in the provider
-                int imagesLength = galleryProvider.galleryImages.length;
-                //These cardWidth and cardHeight variables are created in order
-                //to set the height and width of every image that will be shown.
-                double cardWidth;
-                double cardHeight;
+          Stack(
+            children: [
+             
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(galleryProvider.galleryImages.length,
+                      (index) {
+                    //length of the image list present in the provider
+                    int imagesLength = galleryProvider.galleryImages.length;
+                    //These cardWidth and cardHeight variables are created in order
+                    //to set the height and width of every image that will be shown.
+                    double cardWidth;
+                    double cardHeight;
 
-                //This code written below this comment is used to make images of different heights and widths based on the number of images present in the list.
-                //We have restricted the maximum images to 5.
-                //That's why the logic is not that dynamic.
-                //It'll work only for 5 or less images.
+                    //This code written below this comment is used to make images of different heights and widths based on the number of images present in the list.
+                    //We have restricted the maximum images to 5.
+                    //That's why the logic is not that dynamic.
+                    //It'll work only for 5 or less images.
 
-                if (imagesLength % 2 == 0) {
-                  //If the number of images is even, then we have two cases: either 2 images will be there
-                  //or 4 images will be there because we have restricted the maximum images to 5.
+                    if (imagesLength % 2 == 0) {
+                      //If the number of images is even, then we have two cases: either 2 images will be there
+                      //or 4 images will be there because we have restricted the maximum images to 5.
 
-                  //When two images are there,  it'll set the same width and height for both images.
-                  if (imagesLength == 2) {
-                    cardWidth = defaultEditScaleWidth(containerWidth, 545);
-                    cardHeight = defaultEditScaleHeight(containerHeight, 299);
-                  } else {
-                    //Otherwise, only 1 case remains, in which 4 images are present.
+                      //When two images are there,  it'll set the same width and height for both images.
+                      if (imagesLength == 2) {
+                        cardWidth = defaultEditScaleWidth(containerWidth, 545);
+                        cardHeight =
+                            defaultEditScaleHeight(containerHeight, 299);
+                      } else {
+                        //Otherwise, only 1 case remains, in which 4 images are present.
 
-                    //For that, we have to set the size of the first and last images to be the same
-                    //and the second and third images to be the same.
+                        //For that, we have to set the size of the first and last images to be the same
+                        //and the second and third images to be the same.
 
-                    //It's checking if the index in the list view is first or last, then setting this height and width to the image.
-                    if (index == 0 || index == imagesLength - 1) {
-                      cardWidth = defaultEditScaleWidth(containerWidth, 285);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 236);
+                        //It's checking if the index in the list view is first or last, then setting this height and width to the image.
+                        if (index == 0 || index == imagesLength - 1) {
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 285);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 236);
+                        } else {
+                          //If the index is second or third, then setting this height and width to the image
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 247);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 299);
+                        }
+                      }
                     } else {
-                      //If the index is second or third, then setting this height and width to the image
-                      cardWidth = defaultEditScaleWidth(containerWidth, 247);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 299);
-                    }
-                  }
-                } else {
-                  //If the number of images is odd,then we have three cases: either one image will be there,
-                  //three images will be there, or five images will be there.
+                      //If the number of images is odd,then we have three cases: either one image will be there,
+                      //three images will be there, or five images will be there.
 
-                  //When only one image is there, we'll set this height and width of the image.
-                  if (imagesLength == 1) {
-                    cardWidth = defaultEditScaleWidth(containerWidth, 689);
-                    cardHeight = defaultEditScaleHeight(containerHeight, 379);
-                  }
-                  //Otherwise, we'll check whether the number of images is 3 or 5
-                  //And then apply the logic according to their length.
-                  else if (imagesLength == 3) {
-                    //If the number of images is 3, then we'll set the height and width of the first image to be the same
-                    //and the height and width of the middle or second image to be greater than the other two images.
-                    if (index == 0 || index == imagesLength - 1) {
-                      cardWidth = defaultEditScaleWidth(containerWidth, 300);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 299);
-                    } else {
-                      cardWidth = defaultEditScaleWidth(containerWidth, 438);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 379);
+                      //When only one image is there, we'll set this height and width of the image.
+                      if (imagesLength == 1) {
+                        cardWidth = defaultEditScaleWidth(containerWidth, 689);
+                        cardHeight =
+                            defaultEditScaleHeight(containerHeight, 379);
+                      }
+                      //Otherwise, we'll check whether the number of images is 3 or 5
+                      //And then apply the logic according to their length.
+                      else if (imagesLength == 3) {
+                        //If the number of images is 3, then we'll set the height and width of the first image to be the same
+                        //and the height and width of the middle or second image to be greater than the other two images.
+                        if (index == 0 || index == imagesLength - 1) {
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 300);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 299);
+                        } else {
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 438);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 379);
+                        }
+                      }
+                      //This is the last case in which the number of images is 5.
+                      //In this, the size of the middle image will be larger than the other four images,
+                      //and the size of the images adjacent to the middle image will be smaller than the middle image
+                      //but greater than their adjacent images, i.e., images present at the first and last positions.
+                      else {
+                        //Check if the index of the image is first or last; if yes, then set this size.
+                        if (index == 0 || index == imagesLength - 1) {
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 192);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 236);
+                        }
+                        //Check if the index is second from the start or second from the last.
+                        else if (index == 1 || index == imagesLength - 2) {
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 188);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 299);
+                        }
+                        //if the index is in the middle of the image list
+                        else {
+                          cardWidth =
+                              defaultEditScaleWidth(containerWidth, 267);
+                          cardHeight =
+                              defaultEditScaleHeight(containerHeight, 379);
+                        }
+                      }
                     }
-                  }
-                  //This is the last case in which the number of images is 5.
-                  //In this, the size of the middle image will be larger than the other four images,
-                  //and the size of the images adjacent to the middle image will be smaller than the middle image
-                  //but greater than their adjacent images, i.e., images present at the first and last positions.
-                  else {
-                    //Check if the index of the image is first or last; if yes, then set this size.
-                    if (index == 0 || index == imagesLength - 1) {
-                      cardWidth = defaultEditScaleWidth(containerWidth, 192);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 236);
-                    }
-                    //Check if the index is second from the start or second from the last.
-                    else if (index == 1 || index == imagesLength - 2) {
-                      cardWidth = defaultEditScaleWidth(containerWidth, 188);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 299);
-                    }
-                    //if the index is in the middle of the image list
-                    else {
-                      cardWidth = defaultEditScaleWidth(containerWidth, 267);
-                      cardHeight = defaultEditScaleHeight(containerHeight, 379);
-                    }
-                  }
-                }
-                return Container(
-                  height: cardHeight,
-                  clipBehavior: Clip.hardEdge,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: Image.asset(
-                    galleryProvider.galleryImages[index],
-                    fit: BoxFit.cover,
-                    height: cardHeight,
-                    width: cardWidth,
-                  ),
-                );
-              })),
+                    return Container(
+                      height: cardHeight,
+                      clipBehavior: Clip.hardEdge,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child:galleryProvider.galleryImages[index]=='default'
+                      ?Image.asset(
+                        'assets/images/defaultTemplate/GalleryImage.png',
+                        fit: BoxFit.cover,
+                        height: cardHeight,
+                        width: cardWidth,
+                      )
+                      :Image.memory(
+                       base64Decode(galleryProvider.galleryImages[index]),
+                       fit: BoxFit.cover,
+                        height: cardHeight,
+                        width: cardWidth,
+                       ),
+                    );
+                  })),
+
+                   Align(
+                alignment: Alignment.center,
+                child: InkWell(
+                    onTap: () {
+                      galleryProvider.pickImage();
+                    },
+                    child: Container(
+                      height: defaultEditScaleWidth(containerWidth, 50),
+                      width: defaultEditScaleHeight(containerHeight, 50),
+                      color: Colors.blue,
+                    )),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
+
+  
 }
