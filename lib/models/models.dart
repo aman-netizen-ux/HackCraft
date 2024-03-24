82aa6e79212bcfaa6581a4eb0fdc8fbbeae71ffb
Abha Agarwal <abhaa1023@gmail.com>
2024-03-24 22:49:24 +0530

    custom json structure trial

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,4 +1,31 @@
-//create a new file in domain folder to store any type of list of data
-//later can delete this file (models.dart)
+// //create a new file in domain folder to store any type of list of data
+// //later can delete this file (models.dart)
 
-//later add models, repositories, services,etc.
+// //later add models, repositories, services,etc.
+
+
+
+// column:{
+//   key: GlobalKey()
+//   children:{
+//     container:{
+//        key: GlobalKey(),
+//        properties: {},
+//        child: Text{
+//         key: GlobalKey(),
+//         properties:{},
+//         text: ""
+//        }
+//     },
+//     Text(
+//        key: GlobalKey(),
+//        properties:{},
+//        text: ""
+//      ),
+//     Text(
+//        key: GlobalKey(),
+//        properties:{},
+//        text: ""
+//      )
+//   }
+// }
