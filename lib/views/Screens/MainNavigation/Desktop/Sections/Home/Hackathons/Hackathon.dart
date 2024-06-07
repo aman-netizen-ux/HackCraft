0959c75f0eb975a260e18b7beac2b668b5ef127c
Abha Agarwal <abhaa1023@gmail.com>
2024-06-07 06:14:43 +0530

    template variable added in getallhackathon

--- lib/views/Screens/MainNavigation/Desktop/Sections/Home/Hackathons/Hackathon.dart
+++ lib/views/Screens/MainNavigation/Desktop/Sections/Home/Hackathons/Hackathon.dart
@@ -300,7 +300,7 @@ class _HomeHackathonState extends State<HomeHackathon> {
                                                   
 
                                                   final hackathonType =
-                                                      "Custom";
+                                                      hackathon.template;
 
                                                   if (hackathonType ==
                                                       "Custom") {
